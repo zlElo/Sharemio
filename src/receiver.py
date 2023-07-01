171a412ce706f6e2b83dbb840d73d81f23118a00
zlElo <108819252+zlElo@users.noreply.github.com>
2023-07-01 07:36:53 +0200

    Update receiver.py

--- src/receiver.py
+++ src/receiver.py
@@ -5,53 +5,40 @@ import customtkinter
 
 def receive():
     def openserv():
-        # device's IP address
         SERVER_HOST = "0.0.0.0"
         SERVER_PORT = 5001
-        # receive 4096 bytes each time
         BUFFER_SIZE = 4096
         SEPARATOR = "<SEPARATOR>"
 
-        # create the server socket
-        # TCP socket
         s = socket.socket()
 
-        # bind the socket to our local address
+        
         s.bind((SERVER_HOST, SERVER_PORT))
-
-        # enabling our server to accept connections
-        # 5 here is the number of unaccepted connections that
-        # the system will allow before refusing new connections
         s.listen(5)
         print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
         status.configure(text='Status: up')
         root.update_idletasks()
         root.update()
 
-        # accept connection if there is any
         client_socket, address = s.accept() 
-        # if below code is executed, that means the sender is connected
+    
         print(f"[+] {address} is connected.")
 
-        # receive the file infos
-        # receive using client socket, not server socket
+    
         received = client_socket.recv(BUFFER_SIZE).decode()
         filename, filesize = received.split(SEPARATOR)
-        # remove absolute path if there is
+       
         filename = os.path.basename(filename)
-        # convert to integer
+       
         filesize = int(filesize)
         downloaded = 0
 
         with open(filename, "wb") as f:
             while True:
-                # read 1024 bytes from the socket (receive)
+                
                 bytes_read = client_socket.recv(BUFFER_SIZE)
                 if not bytes_read:    
-                    # nothing is received
-                    # file transmitting is done
                     break
-            # write to the file the bytes we just received
                 f.write(bytes_read)
                 downloaded += len(bytes_read)
                 done_ = int(50*downloaded/filesize)
@@ -60,7 +47,6 @@ def receive():
                 root.update_idletasks()
 
         client_socket.close()
-        # close the server socket
         s.close()
         print('[+] done')
 
